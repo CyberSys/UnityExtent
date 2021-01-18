77ba21f92983ced1da55f36e357ab324dd1fd610
Henry <hp@henryprescott.dev>
2021-01-18 18:25:08 +0000

    WIP movement
    initial stab at some movement not working but a start

--- Gameplay/Assets/Scripts/AgentController.cs
+++ Gameplay/Assets/Scripts/AgentController.cs
@@ -4,25 +4,120 @@ using System.Collections.Generic;
 using UnityEngine;
 public class AgentController : MonoBehaviour
 {
-    public float speed;
-    public float tilt;
-
     private Rigidbody Rigidbody;
     private BoxCollider BoxCollider;
 
     private Vector3 MovementDirection = Vector3.zero;
 
-    private float Speed = 1.0f;
-    private float Tilt = 1.0f;
+    public float Speed = 1.0f;
+    public float Tilt = 1.0f;
 
+    private GridController GridController;
+
+    public enum Direction
+    {
+        Left,
+        Right,
+        Forward,
+        Back
+    }
+
+    public struct MovementAction
+    {
+        public MovementAction(Vector3 currentDirection, KeyCode keyPressed)
+        {
+            NewDirection = Vector3.zero;
+            
+            if (currentDirection == Vector3.left)
+            {
+                if (keyPressed == KeyCode.A)
+                {
+                    NewDirection = Vector3.down;
+                }
+
+                if (keyPressed == KeyCode.D)
+                {
+                    NewDirection = Vector3.up;
+                }
+            }
+            if(currentDirection == Vector3.forward)
+            {
+                if (keyPressed == KeyCode.A)
+                {
+                    NewDirection = Vector3.left;
+                }
+
+                if (keyPressed == KeyCode.D)
+                {
+                    NewDirection = Vector3.right;
+                }
+            }
+            if(currentDirection == Vector3.right)
+            {
+                if (keyPressed == KeyCode.A)
+                {
+                    NewDirection = Vector3.forward;
+                }
+
+                if (keyPressed == KeyCode.D)
+                {
+                    NewDirection = Vector3.down;
+                }
+            }
+            if(currentDirection == Vector3.down)
+            {
+                if (keyPressed == KeyCode.A)
+                {
+                    NewDirection = Vector3.right;
+                }
+
+                if (keyPressed == KeyCode.D)
+                {
+                    NewDirection = Vector3.left;
+                }
+            }
+        }
+
+        public Vector3 NewDirection { get; }
+    }
+
+    private List<MovementAction> movementActions;
+    
+    public struct Cell
+    {
+        public Cell(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; }
+        public int Y { get; }
+
+        public override string ToString() => $"({X}, {Y})";
+    }
+
+    public Cell startCell = new Cell(0,0);
+
+    // Start is called before the first frame update
     private void Start()
     {
         Rigidbody = gameObject.GetComponent(typeof(Rigidbody)) as Rigidbody;
         BoxCollider = gameObject.GetComponent(typeof(BoxCollider)) as BoxCollider;
         
         SetMovementDirection(Vector3.forward);
+        
+        GridController = GameObject.FindObjectOfType<GridController>();
     }
 
+    public void SetStartingCell(int x, int y)
+    {
+        if (GridController == null)
+        {
+            GridController = GameObject.FindObjectOfType<GridController>();
+        }
+        transform.position = GridController.GetCell(x,y).centre;
+    }
     public void SetMovementDirection(Vector3 movement)
     {
         MovementDirection = movement;
@@ -41,11 +136,25 @@ public class AgentController : MonoBehaviour
         // }
         if (Input.GetKey(KeyCode.A))
         {
-            newMovementDirection += Vector3.left;
+            if(MovementDirection == Vector3.left)
+                newMovementDirection += Vector3.down;
+            if(MovementDirection == Vector3.forward)
+                newMovementDirection += Vector3.left;
+            if(MovementDirection == Vector3.right)
+                newMovementDirection += Vector3.forward;
+            if(MovementDirection == Vector3.down)
+                newMovementDirection += Vector3.right;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            newMovementDirection += Vector3.right;
+            if(MovementDirection == Vector3.left)
+                newMovementDirection += Vector3.forward;
+            if(MovementDirection == Vector3.forward)
+                newMovementDirection += Vector3.right;
+            if(MovementDirection == Vector3.right)
+                newMovementDirection += Vector3.down;
+            if(MovementDirection == Vector3.down)
+                newMovementDirection += Vector3.left;
         }
         // if (Input.GetKey(KeyCode.Q))
         // {
@@ -62,6 +171,17 @@ public class AgentController : MonoBehaviour
     {
         if (GetInputTranslationDirection() != Vector3.zero)
             MovementDirection = GetInputTranslationDirection();
+        
+        if(MovementDirection == Vector3.right)
+            transform.rotation = Quaternion.Euler(0,90,0);
+        if(MovementDirection == Vector3.left)
+            transform.rotation = Quaternion.Euler(0,-90,0);
+        if(MovementDirection == Vector3.forward)
+            transform.rotation = Quaternion.Euler(0,0,0);
+        if(MovementDirection == Vector3.down)
+            transform.rotation = Quaternion.Euler(0,180,0);
+        
+        
         // float moveHorizontal = Input.GetAxis ("Horizontal");
         // float moveVertical = Input.GetAxis ("Vertical");
         //
@@ -75,6 +195,6 @@ public class AgentController : MonoBehaviour
             Mathf.Clamp (GetComponent<Rigidbody>().position.z, BoxCollider.bounds.min.z, BoxCollider.bounds.max.z)
         );
 
-        GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -Tilt);
+        //GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -Tilt);
     }
 }
--- Gameplay/Assets/Scripts/GridController.cs
+++ Gameplay/Assets/Scripts/GridController.cs
@@ -1,40 +1,98 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class GridController : MonoBehaviour
 {
-    public Transform PlayerTransform;
+    public GameObject player;
+
+    public GameObject cellPrefab;
+
+    public int rowNumber = 10;
+    public int columnNumber = 10;
 
     public class GridCell
     {
-        public Vector3 Centre;
+        public Vector3 centre;
 
         public GridCell(Vector3 centrePosition)
         {
-            Centre = centrePosition;
+            centre = centrePosition;
         }
     }
 
-    private List<List<GridCell>> Grid;
+    private List<List<GridCell>> _grid = new List<List<GridCell>>();
+
+    public GridCell GetCell(int x, int y)
+    {
+        if (x >= _grid.Count)
+        {
+            throw new InvalidOperationException("X index out of bounds.");
+        }
+
+        if (y >= _grid[x].Count)
+        {   
+            throw new InvalidOperationException("X index out of bounds.");
+        }
+
+        return _grid[x][y];
+    }
+
+    private static GameObject GETChildGameObjectWithName(GameObject parent, string withName) {
+        for (var i = 0; i < parent.transform.childCount; i++)
+        {
+            if (parent.transform.GetChild(i).name == withName)
+                return parent.transform.GetChild(i).gameObject;
+        }
+        
+        return null;
+    }
     
     // Start is called before the first frame update
     void Start()
     {
-        Grid = new List<List<GridCell>>();
+        GameObject gridObject = new GameObject {name = "Grid"};
 
-        for (int i = 0; i < 10; i++)
+        gridObject.transform.parent = this.transform;
+
+        for (int i = 0; i < rowNumber; i++)
         {
+            GameObject rowObject = new GameObject();
+
+            rowObject.transform.parent = gridObject.transform;
+
+            rowObject.name = "" + i;
+            
             List<GridCell> row = new List<GridCell>();
-            for (int j = 0; j < 10; j++)
+            for (int j = 0; j < columnNumber; j++)
             {
-                row.Add(new GridCell(new Vector3(i + 0.5f, 0.0f, j + 0.5f)));
+                GridCell newCell = new GridCell(new Vector3(i + 0.5f, 0.0f, j + 0.5f));
+                row.Add(newCell);
+                
+                GameObject cellPrefab = Instantiate(this.cellPrefab, newCell.centre, Quaternion.identity);
+
+                cellPrefab.transform.parent = rowObject.transform;
+
+                cellPrefab.name = "" + j;
+
+                if (j == rowNumber - 1)
+                {
+                    GETChildGameObjectWithName(cellPrefab, "Top").GetComponent<MeshRenderer>().enabled = true;
+                }
+                
+                if (i == columnNumber - 1)
+                {
+                    GETChildGameObjectWithName(cellPrefab, "Right").GetComponent<MeshRenderer>().enabled = true;
+                }
             }
             
-            Grid.Add(row);
+            _grid.Add(row);
         }
-
-        int test = 0;
+        
+        player.GetComponent<AgentController>().SetStartingCell(0,0);
     }
 
     // Update is called once per frame
